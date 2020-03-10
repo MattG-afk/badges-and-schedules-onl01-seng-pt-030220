de36479af4ab4e36bf113ea583bfd83647dd3009
Matt Gordon <bigmattsize21@gmail.com>
2020-03-10 16:30:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 assign
 end
 def printer(attendees)
-  speakers.each_with_index |attendees|
+  speakers.each_with_index do |attendees|
   puts batch_badge_creator(attendees)
   puts assign_rooms(attendees)
 end
