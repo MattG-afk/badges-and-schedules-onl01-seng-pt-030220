80e0f35cc8a1f43ff6bc8a6bb28f857fd8130e71
Matt Gordon <bigmattsize21@gmail.com>
2020-03-10 15:39:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendee)
+  badges=[]
+  attendee.each do |name|
+    badges<< badge_maker(name)
+end
+badges
+end
+def assign_rooms(attendees)
+  attendee=[]
+  attendees.each_with_index do |attendees,room|
+    "Hello #{attendees}! You'll be assigned to room #{room}"
+end
+end
